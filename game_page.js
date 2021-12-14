34fbf106490095b8ad999d0722c7b798df677a84
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-13 16:40:08 -0800

    debug and future use code

--- game_page.js
+++ game_page.js
@@ -28,4 +28,12 @@ function send(){
     remove_charAt1 = word.replace(charAt1,"_");
     remove_charAt2 = remove_charAt1.replace(charAt2,"_");
     remove_charAt3 = remove_charAt2.replace(charAt3,"_");
+    console.log(remove_charAt3);
+
+    question_word = "<h4 id='word_display'> Q. "+remove_charAt3+"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_word + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("word").value = "";
 }
