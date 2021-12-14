20f0a6bb18b2699ecf496aac15289c62e648f8ac
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-13 16:30:53 -0800

    q/a turns

--- game_page.js
+++ game_page.js
@@ -7,3 +7,5 @@ document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
 document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
+document.getElementById("player_question").innerHTML = "Question Turn: " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn: " + player2_name;
