3fe2de1355f7681ae66ab5f658f80549a4356ebe
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-13 16:28:27 -0800

    track score 2

--- game_page.js
+++ game_page.js
@@ -4,3 +4,6 @@ player1_score = 0;
 player2_score = 0;
 document.getElementById("player1_name").innerHTML = player1_name + " : ";
 document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
