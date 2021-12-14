fef9040a6e25241284ce3800f59e855d6eb224a0
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-13 16:37:51 -0800

    word show and replace

--- game_page.js
+++ game_page.js
@@ -9,3 +9,23 @@ document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
 document.getElementById("player_question").innerHTML = "Question Turn: " + player1_name;
 document.getElementById("player_answer").innerHTML = "Answer Turn: " + player2_name;
+
+function send(){
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    
+    charAt1 = word.charAt(1);
+    console.log(charAt1);
+
+    length_divide = Math.floor(word.length/2);
+    charAt2 = word.charAt(length_divide);
+    console.log(charAt2);
+
+    length_minus_one = word.length - 1;
+    charAt3 = word.charAt(length_minus_one);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1,"_");
+    remove_charAt2 = remove_charAt1.replace(charAt2,"_");
+    remove_charAt3 = remove_charAt2.replace(charAt3,"_");
+}
