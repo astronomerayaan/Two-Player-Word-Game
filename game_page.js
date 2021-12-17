f14a3defcad301cc01aa31bdfc833b42c7e0c41c
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-17 09:47:30 -0800

    debug round 2

--- game_page.js
+++ game_page.js
@@ -65,11 +65,11 @@ if(answer == word){
 
   if(answer_turn == "player1"){
     answer_turn = "player2";
-    document.getElementById("player_answer").innerHTML = "Answer Turn: "+player2_name;
+    document.getElementById("player_answer").innerHTML = "Question Turn: "+player2_name;
   }
   else{
     answer_turn = "player1";
-    document.getElementById("player_question").innerHTML = "Answer Turn: "+player1_name;
+    document.getElementById("player_answer").innerHTML = "Answer Turn: "+player1_name;
   }
   document.getElementById("output").innerHTML = "";
 }
