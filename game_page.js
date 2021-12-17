5e931828e766ea781355860d62906a7f8ff2b0e1
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-17 09:32:24 -0800

    q/a turns

--- game_page.js
+++ game_page.js
@@ -53,3 +53,21 @@ function check(){
     document.getElementById("player2_score").innerHTML = player2_score;
   }
 }
+
+if(question_turn == "player1"){
+  question_turn = "player2";
+  document.getElementById("player_question").innerHTML = "Question Turn: "+player2_name;
+}
+else{
+  question_turn = "player1";
+  document.getElementById("player_question").innerHTML = "Question Turn: "+player1_name;
+}
+
+if(answer_turn == "player1"){
+  answer_turn = "player2";
+  document.getElementById("player_answer").innerHTML = "Answer Turn: "+player2_name;
+}
+else{
+  answer_turn = "player1";
+  document.getElementById("player_question").innerHTML = "Answer Turn: "+player1_name;
+}
