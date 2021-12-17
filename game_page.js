6dc358a59c8b7cd259b079c9137f3ec0cba1604d
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-17 09:41:20 -0800

    debug

--- game_page.js
+++ game_page.js
@@ -43,32 +43,33 @@ function check(){
   get_answer = document.getElementById("input_check_box").value;
   answer = get_answer.toLowerCase();
   console.log("Answer: "+answer);
+if(answer == word){
+    if(answer_turn == "player1"){
+      player1_score = player1_score + 1;
+      document.getElementById("player1_score").innerHTML = player1_score;
+    }
+    else{
+      player2_score = player2_score + 1;
+      document.getElementById("player2_score").innerHTML = player2_score;
+    }
+  }
 
-  if(answer_turn == "player1"){
-    player1_score = player1_score + 1;
-    document.getElementById("player1_score").innerHTML = player1_score;
+  if(question_turn == "player1"){
+    question_turn = "player2";
+    document.getElementById("player_question").innerHTML = "Question Turn: "+player2_name;
   }
   else{
-    player2_score = player2_score + 1;
-    document.getElementById("player2_score").innerHTML = player2_score;
+    question_turn = "player1";
+    document.getElementById("player_question").innerHTML = "Question Turn: "+player1_name;
   }
-}
 
-if(question_turn == "player1"){
-  question_turn = "player2";
-  document.getElementById("player_question").innerHTML = "Question Turn: "+player2_name;
+  if(answer_turn == "player1"){
+    answer_turn = "player2";
+    document.getElementById("player_answer").innerHTML = "Answer Turn: "+player2_name;
+  }
+  else{
+    answer_turn = "player1";
+    document.getElementById("player_question").innerHTML = "Answer Turn: "+player1_name;
+  }
+  document.getElementById("output").innerHTML = "";
 }
-else{
-  question_turn = "player1";
-  document.getElementById("player_question").innerHTML = "Question Turn: "+player1_name;
-}
-
-if(answer_turn == "player1"){
-  answer_turn = "player2";
-  document.getElementById("player_answer").innerHTML = "Answer Turn: "+player2_name;
-}
-else{
-  answer_turn = "player1";
-  document.getElementById("player_question").innerHTML = "Answer Turn: "+player1_name;
-}
-document.getElementById("output").innerHTML = "";
